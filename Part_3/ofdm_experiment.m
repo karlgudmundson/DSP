ef07188c6154f6a014f8d968def7e1e7df7ed6fc
MatthieuFontaine <matthieu.fontaine@student.uclouvain.be>
2017-11-07 16:30:04 +0100

    /!\ the new OFDM exp is ofdm_experiment.m
    Fixing some issues + testing the whole channel with infinite SNR

--- Part_3/ofdm_experiment.m
+++ Part_3/ofdm_experiment.m
@@ -8,14 +8,18 @@ L = 100000; %%% length of randome sequence
 rand_seq = randi([0, 1], L*log2(M),1);
 mapping = 'bin';
 UnitAveragePower = true;
-SNR = 90;
+SNR = 2000;
+N = 12;
 %%%%%%%%%%%% Transmitter TX %%%%%%%%%%%%%%
 mod_vec = qam_mod_2(Nq,rand_seq,mapping,UnitAveragePower);
+[x_serial] = ofdm_mod(mod_vec,N,'prefix',false);
 %%%%%%%%%%% Channel -> AWGN %%%%%%%%%%%%%
-noisy_mod_vec = awgn(mod_vec, SNR);
-scatterplot(noisy_mod_vec);
+%noisy_mod_vec = awgn(mod_vec, SNR);
+%scatterplot(noisy_mod_vec);
+noisy_x_serial = x_serial;
 %%%%%%%%%%% Receiver RX %%%%%%%%%%%%%
-demod_vec = qam_demod(noisy_mod_vec,Nq,mapping,UnitAveragePower);
+[Y] = ofdm_demod(noisy_x_serial,N,'prefix',false,L)
+demod_vec = qam_demod(Y,Nq,mapping,UnitAveragePower);
 %%%%%%%%%% BIT ERROR RATE COMPUTATION %%%%%%%%%%%%%
 [ber] = ber(rand_seq,demod_vec)
 %end
