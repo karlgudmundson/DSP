9645e2957e8f2058a84a2a6d872589081fb5c8bd
MatthieuFontaine <matthieu.fontaine@student.uclouvain.be>
2017-11-22 17:05:12 +0100

    Revert "Test"
    This reverts commit aebeff7fcd625b26d41d8bf06230b0b99956e87b.

--- Milestone2/functions/compute_bk.m
+++
@@ -1,8 +0,0 @@
-function [bk] = compute_bk(eq,gamma,pnk)
-% Computing bk with formula given. eq gives the channel freq. response
-
-Hk = abs(eq);
-bk = floor(log2(1+(Hk.^2)./(gamma.*pnk)));
-bk = (bk>6).*6 + (bk<0).*0 + bk.*(bk>=0 & bk<=6); %to bound bk between 0 and 6 
-end
-
